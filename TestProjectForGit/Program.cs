023c35d5dad4ca1c98cf4a49002564ef32f40f97
Manuel Geraldes <45165444+Nel000@users.noreply.github.com>
2022-03-16 16:05:26 +0000

    Add new message for remote edit

--- TestProjectForGit/Program.cs
+++ TestProjectForGit/Program.cs
@@ -7,6 +7,7 @@ namespace TestProjectForGit
         private static void Main(string[] args)
         {
             Console.WriteLine("I'm a test for git");
+            Console.WriteLine("I've been edited remotely");
         }
     }
 }
